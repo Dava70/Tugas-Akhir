c3dc0ab91f5a6505487318295a1239087e825430
Muhammad Dava Septiana <davaseptiana70@gmail.com>
2025-01-11 00:57:18 +0700

    Add files via upload

--- main1.py
+++ main1.py
@@ -1,23 +1,13 @@
 import cv2
-import numpy as np
 
 # Membaca gambar
-image = cv2.imread('bruno.jpg')
+image = cv2.imread('kucing.jpg')
 
-# Mendapatkan dimensi gambar
-(h, w) = image.shape[:2]
+# Menampilkan gambar
+cv2.imshow('Display Window', image)
 
-# Translasi
-M_translation = np.float32([[1, 0, 50], [0, 1, 100]])
-translated_image = cv2.warpAffine(image, M_translation, (w, h))
-
-# Rotasi 45 derajat
-center = (w // 2, h // 2)
-M_rotation = cv2.getRotationMatrix2D(center, 45, 1.0)
-rotated_image = cv2.warpAffine(image, M_rotation, (w, h))
-
-# Menampilkan hasil
-cv2.imshow('Translated Image', translated_image)
-cv2.imshow('Rotated Image', rotated_image)
+# Menunggu hingga ada input dari keyboard
 cv2.waitKey(0)
+
+# Menutup semua jendela
 cv2.destroyAllWindows()
